c8ceaee58edc8076b9338d9dbf0ee5224708c17e
Muhammad Aoun Shabbir <151518539+muhammadaoun123@users.noreply.github.com>
2024-01-10 14:29:02 +0500

    ok done

--- contact.php
+++ contact.php
@@ -4,6 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
      $name = $_POST['name'];
      $email = $_POST['email'];
      $subject = $_POST['subject'];
+     $comment = $_POST['comment'];
 }else{
     header('location: practice.php');
 }
@@ -11,14 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 // connection with my sql
 $connectwithphp = new mysqli('localhost','root','','first');
 
-if($connectwithphp->connect-error){
+if($connectwithphp->connect_error){
     die('connection failed'.$connectwithphp->connect_error);
-}else{
+} else{
     $mydatastore = $connectwithphp->prepare('insert into ok(name,email,subject,comment)
     values(?,?,?,?)');
     $mydatastore->bind_param('ssss',$name,$email,$subject,$comment);
-    $mydatastore->excecute();
-    echo '<h1 style="align-items: center; display: flex; justify-content:center; font-size: 60px; color:white; height:100vh; background:black; font-family:cursive;" > your form is submitted <br> we will reply you as soon as possible</h1>'
+    $mydatastore->execute();
+    echo '<h1 style="align-items: center; display: flex; justify-content:center; font-size: 60px; color:white; height:100vh; background:black; font-family:cursive;" > your form is submitted <br> we will reply you as soon as possible</h1>';
     $mydatastore->close();
     $connectwithphp->close();
   
