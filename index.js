c883939cd8da10ff3c0f11bb7d5d06bff34b33d6
Muhammad Aoun Shabbir <151518539+muhammadaoun123@users.noreply.github.com>
2024-01-15 19:20:35 +0500

    ok done

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ function HideMenu() {
 
 // library used for animation
 AOS.init({
-
     duration: 2000,
 });
 
+
+// nav on scroll color change
+function changBy() {
+    var navbar = document.getElementById('changeonscroll');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition < 200) {
+        navbar.classList.remove('nav_bar_when_scroll');
+    } else {
+        navbar.classList.add('nav_bar_when_scroll');
+    }
+}
+
+window.addEventListener('scroll', changBy);
